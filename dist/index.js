ab941690c3a7d85776c49e32658a873d345b072b
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2023-01-17 17:24:08 -0600

    Write the commands

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let mensaje = 'hola mundo';
+mensaje = 'hola';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 // run `node index.js` in the terminal
 // console.log(`Hello Node.js v${process.versions.node}!`);
+
+let mensaje: string = 'hola mundo';
+mensaje = 'hola';
